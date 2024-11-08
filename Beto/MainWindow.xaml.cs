743fb6b14b548a2be38feecc2bb53e08ae155f3f
Пегас <@DESKTOP-8MRGA7V>
2024-11-08 18:11:38 +0300

    Добавьте файлы проекта.

---
+++ Beto/MainWindow.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+namespace WpfApp
+{
+    public partial class MainWindow : Window
+    {
+        private int _currentNumber = 0;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void AddButton_Click(object sender, RoutedEventArgs e)
+        {
+            _currentNumber++;
+            NumberListView.Items.Add(_currentNumber);
+        }
+        private void DelButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (NumberListView.SelectedItem != null)
+            {
+                NumberListView.Items.Remove(NumberListView.SelectedItem);
+            }
+            else
+            {
+                MessageBox.Show("Пожалуйста, выберите элемент для удаления.");
+            }
+        }
+    }
+}
